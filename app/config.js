3e07f19bbb71937ae6219eb0490c43143fcbb298
Chris Thomas <christopher.thomas@digital.cabinet-office.gov.uk>
2016-07-26 10:56:28 +0100

    Add type-ahead search to custom grouping page

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@
 module.exports = {
 
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "Performance Platform (or something)",
+  serviceName: "Performance",
 
   // Default port that prototype runs on
   port: '3000',
